f495afb03170d5c063f8deef5f8eb5bb4f1061c8
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2024-08-25 11:03:30 +0200

    more changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { Form } from './components/Form/Form';
-import { Header } from './components/Header/Header';
 import { List } from './components/List/List';
-import { useWebScoket } from './hooks/useWebSocket';
+import { useWebSocket } from './hooks/useWebSocket';
 import './App.scss';
 import { ErrorBanner } from './components/ErrorBanner/ErrorBanner';
 
 function App() {
-    const [isReady, send, stocks, setStocks] = useWebScoket();
+    const [isReady, send, stocks, setStocks] = useWebSocket();
 
     return <div className='container'>
-        <Header />
         <Form isReady={isReady} send={send} stocks={stocks} />
         {!isReady && stocks.length > 0 && <ErrorBanner message='Due to network connection issues the stock prices may not be upto date!' />}
         <List isReady={isReady} stocks={stocks} send={send} setStocks={setStocks} />
